c02bc04677f58a8c7e2b05fbe3e248a537d5354f
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2025-12-01 18:24:37 +1030

    Tidy day 1

--- src/P1.hs
+++ src/P1.hs
@@ -15,9 +15,10 @@ parse = map parseLine . lines
 parseLine :: String -> Int
 parseLine ('R':xs) = read xs
 parseLine ('L':xs) = -(read xs)
+parseLine x = error $ "Bad input: " ++ x
 
 solve1 :: [Int] -> Int
-solve1 = length . filter (== (0)) . map (`mod` 100) . scanl (+) 50
+solve1 = length . filter (== 0) . map (`mod` 100) . scanl (+) 50
 
 solve2 :: [Int] -> Int
 solve2 = countZeros 50
@@ -26,5 +27,5 @@ countZeros :: Integral t => t -> [t] -> t
 countZeros _ [] = 0
 countZeros start (delta:xs) =
     let next = (start + delta)
-        zeros = (abs $ next `quot` 100) + if signum start == -(signum next) || next == 0 then 1 else 0
+        zeros = abs (next `quot` 100) + if signum start == -(signum next) || next == 0 then 1 else 0
     in  zeros + countZeros (next  `mod` 100) xs
