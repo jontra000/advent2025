46dc151eb6387f978d477daac0ec03e510fecde7
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2025-12-03 09:37:01 +1030

    Tidy day 2

--- src/P2.hs
+++ src/P2.hs
@@ -1,37 +1,43 @@
 module P2 (run1, run2, inputLocation) where
 
-import qualified Data.Map as M
-import qualified Data.Set as S
 import Data.List.Split (splitOn, chunksOf)
 
+run1 :: [Char] -> Integer
 run1 = solve1 . parse
 
+run2 :: [Char] -> Integer
 run2 = solve2 . parse
 
 inputLocation :: String
 inputLocation = "inputs/input2"
 
-parse = map show . concatMap parseLine . splitOn ","
+parse :: [Char] -> [String]
+parse = map show . concatMap (parseRange . splitOn "-") . splitOn ","
 
-parseLine :: [Char] -> [Int]
-parseLine s = 
-    let (start:end:_) = map read $ splitOn "-"  s
-    in [start..end]
+parseRange :: [[Char]] -> [Int]
+parseRange [start,end] = [read start..read end]
+parseRange x = error $ "Badly formatted range: " ++ show x
 
-solve1 = sum . map read . filter isInvalid
+solve1 :: [String] -> Integer
+solve1 = sum . map read . filter isInvalid1
 
-isInvalid s =
+isInvalid1 :: String -> Bool
+isInvalid1 s = 
     let size = length s `div` 2
     in  take size s == drop size s
 
-isInvalid2 s = any (isInvalid' s) $ [1..length s `div` 2]
+isInvalid2 :: Eq a => [a] -> Bool
+isInvalid2 s = any (isInvalid s) [1..length s `div` 2]
 
-isInvalid' s x =
+isInvalid :: Eq e => [e] -> Int -> Bool
+isInvalid s x =
     let chunks = chunksOf x s
     in  allEqual chunks
 
+allEqual :: Eq a => [a] -> Bool
 allEqual [] = False
 allEqual [_] = False
 allEqual (x:xs) = all (==x) xs
 
+solve2 :: [String] -> Integer
 solve2 = sum . map read . filter isInvalid2
