5ed1addda75bd1771f9fd9ddb79a174d810c9d85
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2025-12-08 10:40:39 +1030

    Day 7 tidy

--- src/P7.hs
+++ src/P7.hs
@@ -5,20 +5,26 @@ module P7 (run1, run2, inputLocation) where
 import qualified Data.Map as M
 import qualified Data.Set as S
 
+run1 :: String -> Int
 run1 = solve1 . parse
 
+run2 :: String -> Integer
 run2 = solve2 . parse
 
 inputLocation :: String
 inputLocation = "inputs/input7"
 
+parse :: String -> [S.Set Int]
 parse = map parseLine . lines
 
+parseLine :: [Char] -> S.Set Int
 parseLine = S.fromList . map fst . filter ((/= '.') . snd) . zip [0..]
 
+solve1 :: [S.Set Int] -> Int
 solve1 [] = 0
 solve1 (start:rows) = countBeams start rows
 
+countBeams :: S.Set Int -> [S.Set Int] -> Int
 countBeams beams (splitters:xs) =
     let toSplit = S.intersection beams splitters
         splitCount = length toSplit
@@ -29,15 +35,17 @@ countBeams beams (splitters:xs) =
 countBeams _ [] = 0
 
 solve2 :: [S.Set Int] -> Integer
-solve2 (start:rows) = countPaths (head $ S.toList start) $ foldl splitBeams (M.fromList $ map (\i -> (i, 1)) [0..maxWidth]) (reverse rows)
+solve2 (start:rows) = countPaths (head $ S.toList start) $ foldl unsplitBeams (M.fromList $ map (\i -> (i, 1)) [0..maxWidth]) (reverse rows)
     where maxWidth = (1 +) $ maximum $ S.unions rows
 solve2 _ = error "bad input"
 
+countPaths :: Int -> M.Map Int Integer -> Integer
 countPaths loc paths = paths M.! loc
 
-splitBeams :: M.Map Int Integer -> S.Set Int -> M.Map Int Integer
-splitBeams possiblePaths splitters = M.fromList $ map (nextPaths splitters possiblePaths) $ M.keys possiblePaths
+unsplitBeams :: M.Map Int Integer -> S.Set Int -> M.Map Int Integer
+unsplitBeams possiblePaths splitters = M.fromList $ map (nextPaths splitters possiblePaths) $ M.keys possiblePaths
 
+nextPaths :: S.Set Int -> M.Map Int Integer -> Int -> (Int, Integer)
 nextPaths splitters prevPaths location 
     | S.member location splitters = (location, prevPaths M.! (location - 1) + prevPaths M.! (location + 1))
     | otherwise = (location, prevPaths M.! location)
